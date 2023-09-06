eb50d0966baea1eb6e28e242a06774f0b8298b44
Navneet <navneetshree17@gmail.com>
2023-09-06 12:21:01 +0530

    Initialised the Service Interface , Class and added a notes file

---
+++ productservice/src/main/java/dev/navneet/productservice/services/FakestoreProductService.java
@@ -0,0 +1,5 @@
+package dev.navneet.productservice.services;
+
+public class FakestoreProductService implements ProductService{
+// This service class will all have the method implementations to interact with the Fakestore API.
+}
---
+++ productservice/src/main/java/dev/navneet/productservice/services/ProductService.java
@@ -0,0 +1,5 @@
+package dev.navneet.productservice.services;
+
+public interface ProductService {
+    // This service class will all have the methods to interact with the Fakestore API.
+}
