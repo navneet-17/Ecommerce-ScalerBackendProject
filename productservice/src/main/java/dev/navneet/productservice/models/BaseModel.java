b0748937f457ed604ae141292ddd25c31db458a7
Navneet <navneetshree17@gmail.com>
2023-09-18 12:59:54 +0530

    finished UUID generator implementation - final commit for the class

--- productservice/src/main/java/dev/navneet/productservice/models/BaseModel.java
+++ productservice/src/main/java/dev/navneet/productservice/models/BaseModel.java
@@ -1,10 +1,20 @@
 package dev.navneet.productservice.models;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
 
+import java.util.UUID;
+@Getter@Setter
 @MappedSuperclass
 public class BaseModel {
     @Id
-    private Long id;
+    @GeneratedValue(generator = "navneet-uuid")
+    @GenericGenerator(name = "navneet-uuid", strategy = "uuid2")
+    @Column(name = "id", columnDefinition = "binary(16)", nullable = false, updatable = false)
+    private UUID uuid;
 }
