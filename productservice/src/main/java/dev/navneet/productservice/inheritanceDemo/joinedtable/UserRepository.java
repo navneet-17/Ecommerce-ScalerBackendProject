764ee7f784964ba83ebb44dbac516203d963a4ae
Navneet <navneetshree17@gmail.com>
2023-09-20 00:13:29 +0530

    done with the joined table inheritance implementation

---
+++ productservice/src/main/java/dev/navneet/productservice/inheritanceDemo/joinedtable/UserRepository.java
@@ -0,0 +1,12 @@
+package dev.navneet.productservice.inheritanceDemo.joinedtable;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository("jt_ur")
+public interface UserRepository
+        extends JpaRepository<User, Long> {
+
+    @Override
+    <S extends User> S save(S entity);
+}
