c13cb33c193adfb153d5368d56d83fb0f7ea7977
Navneet <navneetshree17@gmail.com>
2023-09-23 16:17:48 +0530

    Modified Category Controller to fecth Category name, tested with Postman

--- productservice/src/main/java/dev/navneet/productservice/controllers/CategoryController.java
+++ productservice/src/main/java/dev/navneet/productservice/controllers/CategoryController.java
@@ -3,6 +3,7 @@ package dev.navneet.productservice.controllers;
 import dev.navneet.productservice.dtos.GenericProductDto;
 import dev.navneet.productservice.exceptions.NotFoundException;
 import dev.navneet.productservice.models.Category;
+import dev.navneet.productservice.services.CategoryService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,8 +15,14 @@ import java.util.List;
 @RestController
 @RequestMapping("/categories")
 public class CategoryController {
+    private CategoryService categoryService;
+    public CategoryController(CategoryService categoryService){
+        this.categoryService = categoryService;
+    }
     @GetMapping("{id}")
-    public Category getCategoryById(@PathVariable("id") String uuid) {
-        return null;
+    public String getCategoryById(@PathVariable("id") String uuid) {
+        Category category = categoryService.getCategoryById(uuid);
+        System.out.println("Fetched Category Name is : "+ category.getName());
+        return category.getName();
     }
 }
