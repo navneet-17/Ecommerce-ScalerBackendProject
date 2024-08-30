ea42c41353f19af088cbd24add5df6e7e56644e7
Navneet <navneetshree17@gmail.com>
2024-08-30 13:38:37 +0530

    Kafka Consumer Done! - sending emails via the email Service Application on sign-up.

---
+++ emailservice/src/main/java/dev/navneet/emailservice/EmailserviceApplication.java
@@ -0,0 +1,13 @@
+package dev.navneet.emailservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmailserviceApplication.class, args);
+    }
+
+}
