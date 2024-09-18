3faeeb8499a01e8e8a6511e9428c534722c5443c
Navneet <navneetshree17@gmail.com>
2024-09-18 13:28:48 +0530

    Implemented the API Gateway + Load Balancer service for the user and product service instances, and fetching resources for the individual services via Load Balancer is working correctly!

--- userservice/src/main/java/dev/navneet/userservice/controllers/UserController.java
+++ userservice/src/main/java/dev/navneet/userservice/controllers/UserController.java
@@ -19,7 +19,7 @@ public class UserController {
         this.userService = userService;
     }
 
-    @GetMapping("")
+    @GetMapping("/")
      public List<UserDto> getAllUsers(){
          return userService.getAllUsers();
     }
